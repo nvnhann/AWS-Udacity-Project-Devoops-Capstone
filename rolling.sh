459ffbc29be1d0e859836116ccfc7b186e3b03c7
Your Name <you@example.com>
2023-09-01 09:22:09 +0000

    nvnhan

--- rolling.sh
+++ rolling.sh
@@ -6,7 +6,6 @@ NEW_IMAGE_NAME=registry.hub.docker.com/nvnhan/devoops-capstone:latest
 kubectl get deployments -o wide
 kubectl set image deployments/${DEPLOYMENT_NAME} \
     ${DEPLOYMENT_NAME}=${NEW_IMAGE_NAME}
-echo
 kubectl get deployments -o wide
 kubectl describe pods | grep -i image
 kubectl get pods -o wide
